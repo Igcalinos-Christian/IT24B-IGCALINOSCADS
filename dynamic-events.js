5bad6f4970182bebc61f434a138a593a2a746224
Igcalinos-Christian <igcalchristian@gmail.com>
2024-10-17 22:00:33 +0800

    made my very own script cuz I got tired of ctrl c + ctrl v-ing sir Cliff's code

--- dynamic-events.js
+++ dynamic-events.js
@@ -1,56 +1,42 @@
-class appletList{
-    constructor(dataURL){
-        this.dataURL = dataURL;
-        this.list = [];
-        this.init();
-    }
+var output = document.getElementById('output');
+var input = document.getElementById('input');
 
-    async init(){
-        await this.fetchData();
-        this.renderList(this.list);
-        this.searchList();
-    }
+let listArray = [];
 
-    async fetchData(){
-        try{
-            const response = await fetch(this.dataURL);
-            this.students = await response.json();
-        }catch (error){
-            console.error('Error fetching data:', error);
+fetch("dynamic-events.json")
+    .then(res => res.json())
+    .then(values => {
+        listArray = values;
+        console.log(listArray);
+    })
+
+let inputVal;
+let inputToLowerCase;
+let appletNameToLowerCase;
+
+let processedInput;
+let processedAppletName;
+
+let appletLinkButton;
+let buttonParameters = `class="btn btn-primary;" id="appletButton;"`
+
+input.addEventListener('input', () => {
+    inputVal = input.value;
+    inputToLowerCase = inputVal.toLowerCase();
+
+    processedInput = inputToLowerCase.replace(/\s+/g,'');
+    console.log(processedInput);
+
+    listArray.forEach(value => {
+        appletNameToLowerCase = value.applet_name.toLowerCase();
+        processedAppletName = appletNameToLowerCase.replace(/\s+/g,'');
+        // console.log(processedAppletName);
+
+        if(processedAppletName.includes(processedInput)){
+            appletLinkButton = "<a href=" + value.applet_link + " " + buttonParameters + ">" + value.applet_name + "</a>";
+            output.innerHTML = appletLinkButton;
+            console.log("matching applet: " + value.applet_name);
+            return;
         }
-    }
-    
-    renderList(list) {
-        const listContainer = document.getElementById('List');
-        listContainer.innerHTML = list.map(list =>
-            `<button class="btn btn-primary" style="margin-top: 15px; width: 25rem;" href="${list.applet_link}">
-                ${list.applet_name}
-            </button><br>`
-        ).join('');
-    }
-    
-
-    searchList(){
-        const searchList = document.getElementById('searchList');
-        const Lcontainer = document.getElementById('searchResults');
-
-        searchList.addEventListener('input', () => {
-            this.searchFilter(searchList.value, Lcontainer);
-        });
-
-        this.renderList(this.list, Lcontainer);
-    }
-
-    searchFilter(query, ListContainer){
-        const filteredResults = this.list.filter(list => {
-            const appletName = `${list.applet_name} ${list.applet_link}`;
-            return appletName.toLowerCase().includes(query.toLowerCase());
-        });
-
-        ListContainer.innerHTML = '';
-
-        this.renderList(filteredResults, ListContainer);
-    }
-}
-
-const list = new appletList('dynamic-events.json');
+    })    
+})
