0c35ae5e6d20f73877814be605edab48ec458828
Igcalinos-Christian <igcalchristian@gmail.com>
2024-10-18 22:13:28 +0800

    made ui changes to accomodate the newly descriptions elements in the json file.

--- dynamic-events.js
+++ dynamic-events.js
@@ -8,13 +8,15 @@ let appletLink = [];
 let divContainer;
 let header;
 let link;
+let appletDescription;
 
 fetch("dynamic-events.json")
     .then(res => res.json())
     .then(values => values.map(value => {
         appletName = value.applet_name;
         appletLink = value.applet_link;
-        divContainer = `<div><h2>${appletName}</h2><a href="${appletLink}">${appletName}</a></div>`
+        appletDescription = value.applet_description;
+        divContainer = `<div class="card"><h2>${appletName}</h2><p>${appletDescription}</p><a href="${appletLink}">${appletName}</a></div>`
         output.innerHTML += divContainer;
         console.log(appletName, appletLink);
     }))
