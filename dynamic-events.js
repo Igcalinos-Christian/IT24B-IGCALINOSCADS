11e8ff921010072be0493947e07616446f8acf0e
Igcalinos-Christian <igcalchristian@gmail.com>
2024-10-18 22:02:15 +0800

    finally  was able to make my applets name and links render in the applet Gallery using 'forEach', and fetching directly from the json file.

--- dynamic-events.js
+++ dynamic-events.js
@@ -1,11 +1,20 @@
 const input = document.getElementById('input');
 const output = document.getElementById('output');
+const cardTemplate = document.getElementById('cardTemplate');
 
-let dataList = [];
+let appletName = [];
+let appletLink = [];
+
+let divContainer;
+let header;
+let link;
 
 fetch("dynamic-events.json")
     .then(res => res.json())
-    .then(values => {
-        dataList = values;
-        console.log(dataList);
-    })
+    .then(values => values.map(value => {
+        appletName = value.applet_name;
+        appletLink = value.applet_link;
+        divContainer = `<div><h2>${appletName}</h2><a href="${appletLink}">${appletName}</a></div>`
+        output.innerHTML += divContainer;
+        console.log(appletName, appletLink);
+    }))
